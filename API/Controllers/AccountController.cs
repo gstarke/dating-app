c1217dca548c824127bbf126ebce970b94b26111
Garret Starke <garret.starke@perficient.com>
2021-08-10 14:45:28 -0500

    completed section 5

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -40,7 +40,7 @@ namespace API.Controllers
 
             return new UserDto
             {
-                UserName = user.UserName,
+                Username = user.UserName,
                 Token = _tokenService.CreateToken(user)
             };
         }
@@ -63,7 +63,7 @@ namespace API.Controllers
 
             return new UserDto
             {
-                UserName = user.UserName,
+                Username = user.UserName,
                 Token = _tokenService.CreateToken(user)
             };
         }
